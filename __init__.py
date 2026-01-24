46fb2e5f4f33b44853a32b5dab1ed5295c98876d
uczensokratesa <160874686+uczensokratesa@users.noreply.github.com>
2026-01-24 17:10:29 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from .nodes import TemporalVAEDecode
+from .nodes import UniversalSmartVAEDecode
 NODE_CLASS_MAPPINGS = { "UniversalSmartVAEDecode": UniversalSmartVAEDecode }
 NODE_DISPLAY_NAME_MAPPINGS = { "UniversalSmartVAEDecode": "UniversalSmartVAEDecode" }
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+
